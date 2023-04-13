e8716053a1b7b1f2cc89771e5ac1ac9c962e946d
sadikov16 <sodiqovu880@gmail.com>
2023-04-13 15:41:08 +0500

    Main

--- login.js
+++ login.js
@@ -1,27 +1,27 @@
-// let elForm = document.querySelector("form")
-// let elInput = document.querySelector("#input")
-// let elP = document.querySelector("#email")
+let elForm = document.querySelector("form")
+let elInput = document.querySelector("#input")
+let elP = document.querySelector("#email")
 
-// elForm.addEventListener("submit", async (evt) => {
-//     evt.preventDefault()
-//     let {email, password} = evt.target.elements
+elForm.addEventListener("submit", async (evt) => {
+    evt.preventDefault()
+    let {email, password} = evt.target.elements
     
-//     let data = await fetch("https://reqres.in/api/login", {
-//     method: "POST",
-//     headers: {
-//         "Content-type": "application/json"
-//     },
-//     body: JSON.stringify({
-//         email: email.value.trim(),
-//         password: password.value.trim()
-//     }),
-// })
-// .then(res => res.json())
-// .then(json => json)
-// .catch(err => console.log(error))
+    let data = await fetch("https://reqres.in/api/login", {
+    method: "POST",
+    headers: {
+        "Content-type": "application/json"
+    },
+    body: JSON.stringify({
+        email: email.value.trim(),
+        password: password.value.trim()
+    }),
+})
+.then(res => res.json())
+.then(json => json)
+.catch(err => console.log(error))
 
-// if(data.token){
-//     localStorage.setItem("token", JSON.stringify(data.token ))
-//     window.location.replace("index.html")
-// }
-// })
+if(data.token){
+    localStorage.setItem("token", JSON.stringify(data.token ))
+    window.location.replace("index.html")
+}
+})
